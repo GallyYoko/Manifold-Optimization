7a7128ad4728f0ba5b62c2e53545574f4dec62a6
GallyYoko <Kern_Kinnara@outlook.com>
2024-09-16 14:17:59 +0800

    add non-monotopy backtrack gradient descent methodadient descent

---
+++ Gradient_Descent_N.m
@@ -0,0 +1,46 @@
+function Gradient_Descent_N(func,gradfunc,x0,t0,k,rho,c,iteration1,method,iteration2,epsilon)
+%{
+
+基于非单调线搜索回退法的梯度下降法
+
+参数: 
+func--------光滑函数
+gradfunc----光滑函数func的梯度
+x0----------流形上的初始点
+t0----------初始步长
+k-----------非单调线搜索列表长度
+rho---------回退幅度
+c-----------Armijo条件的判据
+iteration1--回退法的最大迭代次数
+method------收缩映射的方式
+iteration2--梯度下降法的最大迭代次数
+epsilon-----梯度阈值
+
+%}
+[n,p] = size(x0);
+X = zeros(n,p,k);
+x = x0;
+X(:,:,1) = x;
+results = zeros(iteration2,1);
+for i = 1:iteration2
+    if norm(gradfunc(x),"fro") <= epsilon
+        break;
+    end
+    t = t0;
+    min_i = min(i,k);
+    v = -gradfunc(x);
+    for j = 1:iteration1
+        judgement = (func(Retract(x,t*v,method))<=func(X(:,:,1))+c*t*sum(dot(gradfunc(X(:,:,1)),v)));
+        for ind = 1:min_i % 获得非单调线搜索的判别准则
+            judgement = judgement|(func(Retract(x,t*v,method))<=func(X(:,:,ind))+c*t*sum(dot(gradfunc(X(:,:,ind)),v)));
+        end
+        if judgement
+            break;
+        end
+        t = rho*t;
+    end
+    x = Retract(x,t*v,method);
+    X(:,:,mod(i,k)+1) = x; % 更新非单调线搜索的迭代点列表
+    results(i,1) = func(x);
+end
+plot(results);
--- test.m
+++ test.m
@@ -1,18 +1,23 @@
+%% 固定问题相关参数
 n = 10;
 p = 3;
-seed = 0;
+seed = 5;
 
+%% 生成二次函数及其梯度、黎曼梯度
 [A,B] = Random_Coefficient(n,p,seed);
 func = @(x) trace(x'*A*x-2*x'*B);
-gradfunc = @(x) 2*A*x-2*B;
+nablafunc = @(x) 2*A*x-2*B;
+gradfunc = @(x) nablafunc(x)-x*(x'*nablafunc(x)+nablafunc(x)'*x)/2;
 
+%% 固定算法相关参数
 x0 = Random_Initial(n,p,seed);
-t0 = 1;
+t0 = 10;
 rho = 0.5;
 c = 0.001;
-iteration1 = 100;
+iteration1 = 1000;
 method = "qr";
-iteration2 = 100;
-epsilon = 100*eps;
+iteration2 = 1000;
+epsilon = 10e-8;
 
-Gradient_Descent(func,gradfunc,x0,t0,rho,c,iteration1,method,iteration2,epsilon);
+%% 使用算法
+Gradient_Descent_N(func,gradfunc,x0,t0,10,rho,c,iteration1,method,iteration2,epsilon);
